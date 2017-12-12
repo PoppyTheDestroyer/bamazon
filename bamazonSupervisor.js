f533c487e306b19ce1e18ab9bb7197141efa0119
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-12-11 23:03:45 -0600

    beginning endgame

---
+++ bamazonSupervisor.js
@@ -0,0 +1,73 @@
+const connectObject = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+};
+
+const mySql = require("mysql");
+const inquirer = require("inquirer");
+const connection = mySql.createConnection(connectObject);
+
+connection.connect(function (err) {
+    if (err) {
+        throw err
+    }
+    //console.log(`Connected as ID: ${connection.threadId}`);
+});
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View product sales by department", "Create new department", "Exit"],
+            name: "begin"
+        }
+    ])
+    .then(function (inquirerResponse) {
+        if (inquirerResponse.begin === "View product sales by department") {
+            displayTable();
+        }
+        if (inquirerResponse.begin === "Create new department") {
+            newDept();
+        }
+        if (inquirerResponse.begin === "Exit") {
+            connection.end();
+        }
+    })
+
+function newDept() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Assign an ID to the new department.",
+                name: "departmentNew"
+            },
+            {
+                type: "input",
+                message: "Enter the name of the new department",
+                name: "deptName"
+            },
+            {
+                type: "input",
+                message: "What is the overhead for the new department?",
+                name: "overhead"
+            }
+        ])
+        .then(function (inquirerResponse) {
+            connection.query("INSERT INTO DEPARTMENTS SET ?", {
+                department_id: inquirerResponse.departmentNew,
+                department_name: inquirerResponse.deptName, overhead_costs: inquirerResponse.overhead
+            }, function (err, response) {
+                if (err) {
+                    throw err
+                }
+                console.log("Department ID: " + inquirerResponse.departmentNew + 
+                "\nDepartment Name: " + inquirerResponse.deptName + "\nOverhead: " + inquirerResponse.overhead + "\n");
+            })
+            connection.end();
+    })
+}
